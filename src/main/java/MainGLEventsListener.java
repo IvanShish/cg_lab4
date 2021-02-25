8c572337f5a0efa2a83f6f7a3a3c80f44731bd0c
TSVlad <vladislav.kolmykov@yandex.ru>
2021-02-25 23:39:25 +0300

    Initial commit

---
+++ src/main/java/MainGLEventsListener.java
@@ -0,0 +1,25 @@
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+import java.nio.FloatBuffer;
+
+public class MainGLEventsListener implements GLEventListener {
+
+
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    public void display(GLAutoDrawable drawable) {
+
+    }
+
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+}
