a6b88d4351496b6adc6a0c139f2f16645377ed7e
TSVlad <vladislav.kolmykov@yandex.ru>
2021-02-26 16:06:17 +0300

    Point add

--- src/main/java/MainGLEventsListener.java
+++ src/main/java/MainGLEventsListener.java
@@ -16,10 +16,15 @@ public class MainGLEventsListener implements GLEventListener {
     }
 
     public void display(GLAutoDrawable drawable) {
-
+        GL2 gl = drawable.getGL().getGL2();
+        drawStar(gl, new Point(0, 0, 0), 0.5f);
     }
 
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+
+    private void drawStar(GL2 gl, Point center, float size) {
 
     }
 }
