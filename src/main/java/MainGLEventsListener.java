91ed2f30412767da741f8d01aef268e57f00cafe
TSVlad <vladislav.kolmykov@yandex.ru>
2021-02-26 23:13:46 +0300

    all done

--- src/main/java/MainGLEventsListener.java
+++ src/main/java/MainGLEventsListener.java
@@ -34,24 +34,44 @@ public class MainGLEventsListener implements GLEventListener {
         bottomPoint.y = (center.y + 2 * bottomPoint.y) / (1 + 2);
         bottomPoint.x = (center.x + 2 * bottomPoint.x) / (1 + 2);
 
-//        Color color = new Color(1, 0, 0);
+        Point centerOut = new Point(center.x, center.y, center.z + size);
+        Point topOut = new Point(bottomPoint.x + (bottomPoint.x - center.x) * 1.5f, bottomPoint.y + (bottomPoint.y - center.y) * 1.5f, bottomPoint.z + size);
+        Point bottomOut = new Point(topPoint.x + (topPoint.x - center.x) * 0.4f, topPoint.y + (topOut.y - center.y) * 0.4f, topPoint.z + size);
+
+        Color firstColor = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+        Color color = new Color(firstColor.r/10, firstColor.g/10, firstColor.b/10);
+        Color angle = color;
+
+        Color secondColor = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+        Color colorOut = new Color(secondColor.r/10, secondColor.g/10, secondColor.b/10);
+        Color angleOut = colorOut;
 
 
         for (int i = 0; i < 10; i++) {
-            gl.glColor3f((float)Math.random(), (float)Math.random(), (float)Math.random());
+            gl.glColor3f(color.r, color.g, color.b);
             gl.glVertex3f(center.x, center.y, center.z);
             gl.glVertex3f(topPoint.x, topPoint.y, topPoint.z);
             gl.glVertex3f(bottomPoint.x, bottomPoint.y, bottomPoint.z);
 
+            gl.glColor3f(colorOut.r, colorOut.g, colorOut.b);
+            gl.glVertex3f(centerOut.x, centerOut.y, centerOut.z);
+            gl.glVertex3f(topOut.x, topOut.y, topOut.z);
+            gl.glVertex3f(bottomOut.x, bottomOut.y, bottomOut.z);
+
             if (i%2 == 0) {
-                if (deep < 5) {
+                if (deep < 2) {
                     drawStar(gl, topPoint, size / 3, deep + 1);
                 }
                 topPoint = turnPoint(topPoint, center, (float)Math.PI * 2/5);
+                bottomOut = turnPoint(bottomOut, centerOut, (float)Math.PI * 2/5);
+
             } else {
                 bottomPoint = turnPoint(bottomPoint, center, (float)Math.PI * 2/5);
+
+                topOut = turnPoint(topOut, centerOut, (float)Math.PI * 2/5);
             }
-//            color = turnColor(color, 1);
+            color = turnColor(color, angle);
+            colorOut = turnColor(colorOut, angleOut);
         }
 
     }
@@ -64,7 +84,11 @@ public class MainGLEventsListener implements GLEventListener {
         return res;
     }
 
-    private Color turnColor(Color color, float angle) {
-        return new Color();
+    private Color turnColor(Color color, Color angle) {
+        Color res = new Color();
+        res.r = color.r + angle.r;
+        res.g = color.g + angle.g;
+        res.b = color.b + angle.b;
+        return res;
     }
 }
